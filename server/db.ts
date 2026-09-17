217003777517cbb9c119607691343451f9f39b23
iamacompscikid-90 <sofia_cafagna@horacemann.org>
2026-09-17 09:03:45 -0400

    Changed question 1 (100)

--- server/db.ts
+++ server/db.ts
@@ -6,9 +6,9 @@ const sortQuestions = (questions: { points: number; question: string; answer: st
 const pastQuestions: Question[] = sortQuestions([
     {
         points: 100,
-        question: 'What US state is the Land of Lincoln?',
-        answer: 'Illinois',
-    },
+        question: 
+    },'Where was pizza invented',
+        answer: 'Italy',
     {
         points: 200,
         question:
