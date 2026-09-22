63ac3f7fe45cdb10f8bb2104b4faf6e96d11bde9
iamacompscikid-90 <sofia_cafagna@horacemann.org>
2026-09-22 08:44:13 -0400

    Switched around questions to ensure difficulty

--- server/db.ts
+++ server/db.ts
@@ -5,27 +5,27 @@ const TIME_LEFT = 8; // seconds
 const sortQuestions = (questions: { points: number; question: string; answer: string; imgSrc?: string; }[]) => questions.sort((a, b) => a.points - b.points).map(q => ({ ...q, answered: false, buzzers: [] as string[] }));
 const pastQuestions: Question[] = sortQuestions([
     {
-        points: 100,
+        points: 200,
         question: 'Where was pizza invented',
         imgSrc: "pizza-pic.jpeg",
         answer: 'Italy',
     },
     {
-        points: 200,
+        points: 100,
         question:
             'Which country\'s flag is this?',
         imgSrc: "france-flag.jpg",
         answer: 'France',
     },
     {
-        points: 300,
+        points: 400,
         question:
             'What square / plaza of Manhattan is this?',
             imgSrc: "union-square.jpg",
         answer: 'Union Square',
     },
     {
-        points: 400,
+        points: 300,
         question: 'What does Philosophia mean?',
         imgSrc: "athena.jpeg",
         answer: 'Love of wisdom',
@@ -36,25 +36,25 @@ const presentQuestions: Question[] =
     sortQuestions([
         {
             points: 400,
-            question: 'What is the second fastest moving objecct in the Olympics?',
+            question: 'What is the second fastest moving object in the Olympics?',
             imgSrc: "fencing.jpg",
-            answer: 'Tip of a Fencing Blade',
-        },
-        {
-            points: 100,
-            question:
-                'What genre of music originated in the Bronx in the 1970s?',
-            imgSrc: "hiphop.jpg",
-            answer: 'Hip Hop & Rap',
+            answer: 'A Fencing Blade',
         },
         {
             points: 200,
+            question:
+                'What genre of music originated in the Bronx in the 1970s?',
+            imgSrc: "hiphop.jpg",
+            answer: 'Hip Hop',
+        },
+        {
+            points: 300,
             question: 'What famous math problem is this?',
             imgSrc: "collatzconjecture.jpg",
             answer: 'Collatz Conjecture',
         },
         {
-            points: 300,
+            points: 100,
             question:
                 'What type of car is this?',
             imgSrc:
@@ -64,17 +64,17 @@ const presentQuestions: Question[] =
     ]);
 const futureQuestions: Question[] = sortQuestions([
     {
-        points: 100,
+        points: 200,
         question:
-            'This famous activist captured the attention of the world when she was only 15 years old?',
+            'Which famous activist captured the attention of the world when she was only 15 years old?',
         imgSrc:
             "greta.jpeg",
         answer: 'Greta Thunberg',
     },
     {
-        points: 200,
+        points: 400,
         question: 
-        'According to the US National Science Foundation, roughly 25% of the US workforce is in this field',
+        'What job contains roughly 25% of the US workforce?',
         imgSrc: "stem.jpg",
         answer: 'STEM',
 
@@ -82,13 +82,13 @@ const futureQuestions: Question[] = sortQuestions([
     {
         points: 300,
         question: 'The Largest Walt Disney World Resort resides in this city.',
-        imgSrc: "orlando.jpg",
+        imgSrc: "orland.jpg",
         answer: 'Orlando, Florida',
     },{
-        points: 400,
-        question: 'Where can you use this card?',
+        points: 100,
+        question: 'In what city can you use this card?',
         imgSrc: "omny.jpg",
-        answer: 'New York City Subway',
+        answer: 'New York City',
     }
 ]);
 
